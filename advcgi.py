7ad72613d5e90aa696753f91dc739c7e120426a1
root <root@debian>
2014-02-26 09:40:23 +0800

    Add a adv page py file

---
+++ advcgi.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+from cgi import FieldStorage
+from os import environ
+from cStringIO import StringIO
+from urllib import quote, unquote
+from string import capwords, strip, split, join
