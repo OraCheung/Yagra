0bd80027adc31455de5bf08214d4efb0a1210088
root <root@debian>
2014-03-05 03:06:08 +0800

    last version forgive to add advcgi.py

--- advcgi.py
+++ advcgi.py
@@ -5,12 +5,14 @@ from os import environ
 from cStringIO import StringIO
 from urllib import quote, unquote
 from string import capwords, strip, split, join
-import hashlib
 
+from md5encry import MyMd5
 import db
 
+
 class AdvCGI:
-'a new class for user information input and show'
+    "a new class for register infomation set and show"
+
     header = 'Content-Type: text/html\n\n'
     url = '/cgi-bin/advcgi.py'
     login_url = '/cgi-bin/login.py'
@@ -30,8 +32,27 @@ Welcome to Yagra</TITLE></HEAD>
 <P><INPUT TYPE=submit>
 </FORM></BODY></HTML>'''
 
+    reshtml = '''<HTML><HEAD><TITLE>
+Welcome to Yagra</TITLE></HEAD>
+<BODY><H2>Your Register Informations</H2>
+<H3>Your name is: <B>%s</B></H3>
+<H3>Your password is: <B>%s</B></H3>
+<IMG SRC="/cgi-bin/image/%s.jpg" width = '256' height = '256'>
+<H3>Your upload file...<BR>
+Name:<I>%s</I><BR></H3>
+Click <A HREF="%s"><B>here</B></A> to return to login.
+</BODY></HTML>'''
+
+    errhtml = '''<HTML><HEAD><TITLE>
+Welcome to Yagra</TITLE></HEAD>
+<BODY><H3>ERROR</H3>
+<B>%s</B><P>
+<FORM><INPUT TYPE=button VALUE=Back ONCLICK="window.history.back()"></FORM>
+</BODY></HTML>'''
+
 
     def get_cpp_cookies(self):
+        " get the cpp cookies for register"
         if environ.has_key('HTTP_COOKIE'):
             for each_cookie in map(strip, split(environ['HTTP_COOKIE'], ';')):
                 if len(each_cookie) > 6 and each_cookie[:3] == 'CPP':
@@ -50,6 +71,7 @@ Welcome to Yagra</TITLE></HEAD>
             self.passwd = self.passwd2 = self.fn = ''
 
     def show_form(self):
+        "show the form for filling in the information"
         self.get_cpp_cookies()
 
         if not self.cookies.has_key('user') or self.cookies['user'] == '':
@@ -61,37 +83,21 @@ Welcome to Yagra</TITLE></HEAD>
         print AdvCGI.header + AdvCGI.formhtml % (AdvCGI.url,\
                    user_cook, self.passwd, self.passwd2, self.fn)
 
-    errhtml = '''<HTML><HEAD><TITLE>
-Welcome to Yagra</TITLE></HEAD>
-<BODY><H3>ERROR</H3>
-<B>%s</B><P>
-<FORM><INPUT TYPE=button VALUE=Back ONCLICK="window.history.back()"></FORM>
-</BODY></HTML>'''
-
     def show_error(self):
+        " show the error information for user error inputing"
         print AdvCGI.header + AdvCGI.errhtml % (self.error)
 
-    reshtml = '''<HTML><HEAD><TITLE>
-Welcome to Yagra</TITLE></HEAD>
-<BODY><H2>Your Register Informations</H2>
-<H3>Your name is: <B>%s</B></H3>
-<H3>Your password is: <B>%s</B></H3>
-<IMG SRC="/cgi-bin/image/%s.jpg" width = '256' height = '256'>
-<H3>Your upload file...<BR>
-Name:<I>%s</I><BR></H3>
-Click <A HREF="%s"><B>here</B></A> to return to login.
-</BODY></HTML>'''
-
     def set_cpp_cookies(self):
+       "set cpp cookies"
        for each_cookies in self.cookies.keys():
            print 'Set-Cookie: CPP%s=%s; path=/' % \
                (each_cookies, quote(self.cookies[each_cookies]))
 
     def do_results(self):
-        m = hashlib.md5()
+        "show the results for user infromation filled in the form"
         image_name = self.user + '@' + self.passwd
-        m.update(image_name)
-        hash_image = m.hexdigest() 
+        md5 = MyMd5(image_name)
+        hash_image = md5.get_hex() 
         path = '/var/www/cgi-bin/image/%s.jpg' % (hash_image)
         file = open(path, 'wb+')
         file.write(self.fp.read())
@@ -112,15 +118,18 @@ Click <A HREF="%s"><B>here</B></A> to return to login.
                                      hash_image, file_name,  AdvCGI.login_url)
 
     def insertDB(self):
+        "insert information for MySql"
         python_db = db.MyDB()
         python_db.insert_user(self.user, self.passwd) 
 
     def checkDB(self):
+        "check the Mysql if user name is exists"
         python_db = db.MyDB()
         result = python_db.check_name(self.user)
         return result
 
     def go(self):
+        "run the AdvCGI and show in the web"
         self.cookies = {}
         self.error = ''
         form = FieldStorage()
